1e82e5719c9bb87c55b6f531f5574f796254f882
harichandana1810 <52564706+harichandana1810@users.noreply.github.com>
2021-08-10 11:08:28 +0530

    Create RegisterOptions.php

---
+++ RegisterOptions.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Selling &mdash; Website by Colorlib</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/fonts/icomoon/style.css">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/css/jquery-ui.css">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/css/owl.carousel.min.css">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/css/jquery.fancybox.min.css">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/css/bootstrap-datepicker.css">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/fonts/flaticon/font/flaticon.css">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/css/aos.css">
+<link rel="stylesheet" href="https://colorlib.com/preview/theme/selling/css/style.css">
+<style>
+.button {
+background-color: #1c87c9;
+border: none;
+color: white;
+padding: 20px 34px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 20px;
+margin: 4px 2px;
+cursor: pointer;
+}
+</style>
+</head>
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+<div class="site-wrap">
+<div class="site-mobile-menu site-navbar-target">
+<div class="site-mobile-menu-header">
+<div class="site-mobile-menu-close mt-3">
+<span class="icon-close2 js-menu-toggle"></span>
+</div>
+</div>
+<div class="site-mobile-menu-body"></div>
+</div>
+<div class="top-bar py-3 bg-light" id="home-section">
+<div class="container">
+<div class="row align-items-center">
+</div>
+</div>
+</div>
+<header class="site-navbar py-4 bg-white js-sticky-header site-navbar-target" role="banner">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-6 col-xl-2">
+<h1 class="mb-0 site-logo"><a href="index.html" class="text-black mb-0">LOCAL DEALS<span class="text-primary">.</span> </a></h1>
+</div>
+<div class="col-12 col-md-10 d-none d-xl-block">
+<nav class="site-navigation position-relative text-right" role="navigation">
+<ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+<li><a href="#home-section" class="nav-link">Home</a></li>
+<li><a href="#products-section" class="nav-link">Products</a></li>
+<li><a href="#about-section" class="nav-link">About Us</a></li>
+<li><a href="#special-section" class="nav-link">Special</a></li>
+
+</nav>
+</div>
+
+<div class="header">
+<h2>Register</h2>
+</div>
+
+<div><a href="Customer/register.php" class="button">Register as customer</a></div>
+<div><a href="Retailer/register.php" class="button">Register as Shopkeeper</a></div>
+</body>
+</html>
